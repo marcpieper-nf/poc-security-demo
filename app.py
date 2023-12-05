6bc631a5b1d0a4e04d10e951019b5ef34ac91e71
Marc Pieper <149369309+marcpieper-nf@users.noreply.github.com>
2023-12-05 11:47:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World from a simple Python Flask app!'
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+print("Bitcoin ftw!")
