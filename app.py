f429f56006532d4702ff21354f637af003be241f
Marc Pieper <149369309+marcpieper-nf@users.noreply.github.com>
2023-12-06 13:45:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("Bitcoin ftw!")
+import os
+
+# Insecure function that takes a command from user input
+def execute_command(user_input):
+    # Using os.system with user input is a security risk
+    os.system(user_input)
+
+# Example usage of the insecure function
+execute_command("ls")
