b16d053d01073f7a673a4622164c7f0db45a9632
Marc Pieper <149369309+marcpieper-nf@users.noreply.github.com>
2023-12-06 13:50:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 import os
+import requests
 
-# Insecure function that takes a command from user input
+# Insecure function that takes a command from user input for execution
 def execute_command(user_input):
     # Using os.system with user input is a security risk
     os.system(user_input)
 
-# Example usage of the insecure function
-execute_command("ls")
+# Function vulnerable to Server-Side Request Forgery (SSRF)
+def fetch_url(user_url):
+    # Directly using user input to make a web request
+    response = requests.get(user_url)
+    return response.text
+
+# Example usage of the insecure functions
+execute_command("ls")  # Example of command execution vulnerability
+print(fetch_url("http://example.com"))  # Example of SSRF vulnerability
